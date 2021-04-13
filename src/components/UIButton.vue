0036cbc1169b4c5e46ba20c6bb5d6234cd75ab5e
cobraguy <cobraguy@users.noreply.github.com>
2021-04-13 12:35:46 -0500

    Add custom button to normalize styling with FileUpload

---
+++ src/components/UIButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="ui-button"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UIButton',
+});
+</script>
+
+<style>
+.ui-button {
+  background-color: #500000;
+  border: none;
+  color: white;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1em;
+  padding: 0.5em;
+}
+
+.ui-button:hover {
+  color: grey;
+  cursor: pointer;
+}
+
+@media (prefers-color-scheme: dark) {
+  .ui-button {
+    background-color: #81302b;
+  }
+}
+</style>
