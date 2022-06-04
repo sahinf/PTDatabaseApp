baf060380e89fb74bc9e86d32faef255ff6d470b
furkan <furkansahin824@gmail.com>
2022-06-03 19:34:33 -0500

    Reusable Card and Upload button, FileUpload page done for now

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title = "";
+    export let desc = "";
+</script>
+
+<div class="card w-96 bg-primary text-primary-content">
+    <div class="card-body">
+        <h2 class="card-title">{title}</h2>
+        <p>{desc}</p>
+        <div class="card-actions justify-end">
+            <slot/>
+        </div>
+    </div>
+</div>
