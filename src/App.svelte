783b7337b6cba18a92aa1f2b5ff01fd68a1361be
furkan <furkansahin824@gmail.com>
2022-06-05 16:37:02 -0500

    Remove custom css, use inline

--- src/App.svelte
+++ src/App.svelte
@@ -18,29 +18,5 @@
 
 	/* Custom Styles */
 	@layer utilities {
-		.assign-box {
-			/* w-1/3 bg-green-500 flex flex-col border-solid hover:border-4 border-2 border-slate-400 */
-			width: 33.33%;
-			display: flex;
-			flex-direction: column;
-			border-width: 4px;
-			border: solid;
-			border-color: rgb(148 163 184);
-		}
-		.assign-box:hover {
-			border-width: 6px;
-			border-color: black;
-		}
-
-		.assign-box-header {
-			font-size: 1.5rem;
-			font-family: sans;
-			border: solid;
-			text-align: center;
-			height: 10%;
-			overflow: hidden;
-		}
-
-
 	}
 </style>
