22840b552cbbc1b1d1c0a960e7a281dcfd547484
Furkan Sahin <furkansahin@tamu.edu>
2022-07-21 09:05:18 -0400

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()]
-  // plugins: [svelte({
-  //   preprocess: preprocess()
-  //   // hot: {
-  //   //   // preserveLocalState
-  //   // }
-  // })]
 })
