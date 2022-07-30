3f86bf0ae06eb61d07ebde4ff7928ace94807d62
Furkan Sahin <furkansahin@tamu.edu>
2022-07-30 17:31:25 -0500

    Downloading `csv` file now also displays lab faculty info

--- src/components/Labs.svelte
+++ src/components/Labs.svelte
@@ -49,9 +49,9 @@
     let csv = cols.join(",") + "\n";
     labsAndPts.forEach((row) => {
       let l = row.lab;
-      csv += `${l.course},${l.section},${l.time},${l.location},${
-        row.pt?.name ?? "UNASSIGNED"
-      }\n`;
+      csv += `${l.course},${l.section},${l.time},${l.location},${displayFaculty(
+        l
+      )},${row.pt?.name ?? "UNASSIGNED"}\n`;
     });
     console.log(csv);
 
