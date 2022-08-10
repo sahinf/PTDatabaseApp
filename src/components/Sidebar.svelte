02cf5e271206245c0e9cafab4abc1620159ac128
furkan <furkansahin824@gmail.com>
2022-08-10 10:36:27 -0500

    `style`: Stop displaying unused sections

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -7,42 +7,34 @@
   import TamuOutput from "./TamuOutput.svelte";
   import { onMount } from "svelte";
   import { parseDatabaseLocalStorage } from "../util/parser";
-  import Icon from "../components/helpers/Icon.svelte";
   let sections = [
-    { name: "File Uploads", component: FileUploads, icon: "checkmark" },
-    { name: "Peer Teachers", component: PeerTeachers, icon: "checkmark" },
-    { name: "Assign Labs", component: AssignLabs, icon: "checkmark" },
-    { name: "Labs", component: Labs, icon: "checkmark" },
-    { name: "Active Peer Teachers", component: null }, // TODO
-    { name: "Stats", component: null }, // TODO
-    { name: "TAMU HTML Output", component: TamuOutput, icon: "checkmark" },
+    { name: "File Uploads", component: FileUploads },
+    { name: "Peer Teachers", component: PeerTeachers },
+    { name: "Assign Labs", component: AssignLabs },
+    { name: "Labs", component: Labs },
+    { name: "TAMU HTML Output", component: TamuOutput },
   ];
 
   let selected = sections[0];
 
-  // Load from local storage. FOR TESTING PURPOSES ONLY. REMOVE THIS FROM PRODUCTION
+  // Load from local storage
   onMount(() => {
     const db = localStorage.getItem("db");
     if (db) {
       console.log("Using database found in local storage");
       parseDatabaseLocalStorage(db);
-    } else {
-      console.log("No database found in local storage");
     }
   });
-
-  $: console.log(selected.icon);
 </script>
 
 <!-- Entire Page -->
 <div class="flex flex-row h-screen">
-  <!-- SIDEBAR -->
+  <!-- Sidebar -->
   <div class="flex-none w-2/12 flex-col border-r">
     <!-- Header for sidebar sections -->
     <div
       class="font-serif flex-none text-center text-3xl p-1 border-b font-black overflow-hidden"
     >
-      <!-- Text -->
       <div
         class="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
       >
@@ -60,16 +52,9 @@
           <li>
             <div
               class={selected == sec ? "active" : ""}
-              on:click={() => {
-                selected = sec;
-              }}
+              on:click={() => (selected = sec)}
             >
-              <div>
-                {sec.name}
-              </div>
-              <div>
-                <Icon name={sec.icon} />
-              </div>
+              {sec.name}
             </div>
           </li>
         {/each}
